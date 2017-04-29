d7b27b94ba08f3e128d56480127c27587453a5f2
Manoj Nama <manoj.nama@outlook.com>
2017-04-29 12:12:10 +0530

    Updating schema for Events, adding indexes

--- models/Event/schema.js
+++ models/Event/schema.js
@@ -1,18 +1,23 @@
+const mongoose = require('mongoose');
+const eventSchema = require('mongoose').Schema;
 
- const eventSchema = require('mongoose').Schema;
- 
-  module.exports = new eventSchema({
-      name: { type: String, required: true},
-      location: {
-          lat: {type: Number},
-          lng: {type: Number}
-      },
-      city: { type: String, required: true},
-      organisedBy: { type: String, required: true},
-      openEvent: {type: Number},
-      source: {type: String},
-      date: {type: Date},
-      charges: {type: String}
-  });
-      
-      
+const schema = new eventSchema({
+    name: { type: String, required: true },
+    createdAt: {type: Number, default: Date.now},
+    location: {
+        lat: { type: Number },
+        lng: { type: Number }
+    },
+    city: { type: String, required: true },
+    organisedBy: { type: String, required: true },
+    openEvent: { type: Number },
+    source: { type: String },
+    date: { type: Number },
+    charges: { type: String }
+});
+
+schema.index({createdAt: 1});
+
+const eventModel = mongoose.model("Event", schema);
+module.exports = eventModel;
+
