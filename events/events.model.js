bd1a09e984641852394e84ec3000aea842b42353
Akash Yadav <akash0992@gmail.com>
2017-04-29 16:05:14 +0530

    schema changes

--- events/events.model.js
+++ events/events.model.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const eventSchema = require('mongoose').Schema;
 
 const schema = new eventSchema({
-    eventName: { type: String, required: true },
-    createdAt: {type: Number, default: Date.now},
+    eventName: { type: String},
+    createdAt: {type: Date, default: Date.now},
     location: {
         lat: { type: Number },
         lng: { type: Number }
     },
-    city: { type: String, required: true },
-    organisedBy: { type: String, required: true },
+    city: { type: String },
+    organisedBy: { type: String},
     openEvent: { type: Boolean },
     source: { type: String },
-    date: { type: Number },
+    date: { type: Date },
     charges: { type: String },
     description: {type: String},
     phone: {type: String},
