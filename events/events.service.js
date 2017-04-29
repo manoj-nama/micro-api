18fd8ec4e107180026d694e0c3f43fdc736f0cfc
Manoj Nama <manoj.nama@outlook.com>
2017-04-29 14:58:19 +0530

    Adding Procfile

--- events/events.service.js
+++ events/events.service.js
@@ -1,14 +1,16 @@
 const EventModel = require('./events.model');
 
 module.exports = {
-    fetchEventList : (payload, CB) => {
-
-        EventModel.find(payload, (err, data) => {
-           if (err) {
-               CB(err, null);
-           } else {
-              CB(null, data);
-           }
-        });
+    fetchEventList: (payload, CB) => {
+        EventModel
+            .find(payload)
+            .lean()
+            .exec((err, data) => {
+                if (err) {
+                    CB(err, null);
+                } else {
+                    CB(null, data);
+                }
+            });
     }
 };
