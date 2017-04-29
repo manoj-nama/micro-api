75fe5fa7f4101e3edd3b7598615d8672da54ab7e
rachit2212 <rachit2212@gmail.com>
2017-04-29 13:01:58 +0530

    Routing mechanism added

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
 
+
 app.use((req, res, next) => {
   console.log(req.method + ": " + req.url);
   next();
 });
 
 mongoose.connect(config.mongo_uri);
+var models = require('./events/models');
 
 app.use(bodyParser.json({limit: '16mb'}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// setup routing handlers
+var routes = require('./events/controller')({ models });
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
