587fff0afb7dfb624f9ad86db516e25a6296f87b
Manoj Nama <manoj.nama@outlook.com>
2017-04-29 12:43:35 +0530

    Adding mongolab credentials

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const config = {
   mongo: {
     host: "ds123371.mlab.com:23371/heroku_x1gzz76r",
-    user: "",
-    password: ""
+    user: "microservice-integration",
+    password: "igdefault"
   }
 };
 
